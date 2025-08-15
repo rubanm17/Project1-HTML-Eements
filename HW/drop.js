537f5c73938d9db15e1bbac56f8039e11a51532d
Ruban <trmgaming02@gmail.com>
2025-08-15 11:51:12 +0530

    HW

---
+++ HW/drop.js
@@ -0,0 +1,6 @@
+//Function to transfer one value to another
+function input() {
+    let a = 4;
+    document.getElementById("demo").innerHTML = "The value of the input is " + a;    
+    document.getElementById("demo1").innerHTML = "The value of the new node of "+ a;    
+}
