65eedfff2191ce1b13a071399385e08d5ec72d79
Ruban <trmgaming02@gmail.com>
2025-10-22 11:51:49 +0530

    Python

--- Python/nl.py
+++ Python/nl.py
@@ -5,10 +5,10 @@ def test(st):
     return result
 
 students = [
-    [1, 'Jean Castro', 'V']
-    [2,'Lula Powell','V']
-    [3, 'Brian Howell','VI']
-    [4,'Lynne Foster','VI']
+    [1, 'Jean Castro', 'V'],
+    [2,'Lula Powell','V'],
+    [3, 'Brian Howell','VI'],
+    [4,'Lynne Foster','VI'],
     [5,'Zachary Simon','VII']
 ]
 print("\nOriginal list of lists:")
