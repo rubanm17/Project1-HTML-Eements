9e10f57ee6a701148c2fdd36927cd2ea88b98e69
Ruban <trmgaming02@gmail.com>
2025-11-06 20:34:58 +0530

    HW_Python

--- HW_Python/rgsc.py
+++ HW_Python/rgsc.py
@@ -1,35 +1,54 @@
-class shapes:
-    def area(self):
-        pass
-    
-class rectangle(shapes):
-    def __init__(self,length,breadth):
+class Rectangle :
+    def __init__(self, length, breadth) :
         self.length = length
         self.breadth = breadth
 
-    def area(self):
-        return self.length * self.breadth
-    
-class triangle(shapes):
-    def __init__(self,length,breadth):
+    def area(self) :
+        a = self.length * self.breadth    
+        print("Area of the Rectangle : ", a)
+
+    def data(self) :
+        print("Length : ",self.length, "and Breadth :", self.breadth) 
+
+class Triangle :
+    def __init__(self, length, breadth) :
         self.length = length
         self.breadth = breadth
 
-    def area(self):
-        return 0.5 * self.length * self.breadth
-    
-class square(shapes):
-    def __init__(self, side):
+    def area(self) :
+        b = 0.5 * self.length * self.breadth    
+        print("Area of the Triangle : ", b)
+
+    def data(self) :
+        print("Length : ",self.length, "and Breadth :", self.breadth)           
+
+class Square :
+    def __init__(self, side) :
         self.side = side
 
-    def area(self):
-        return self.side ** 2
+    def area(self) :
+        c = self.side ** 2    
+        print("Area of the Square : ", c)
 
-r = rectangle(5,10)
-print("Area of the Rectangle : ", r.area())
+    def data(self) :
+        print("Side : ",self.side) 
 
-t = triangle(5,10)
-print("Area of the Triangle : ", t.area())
+class Circle :
+    def __init__(self, radius) :
+        self.radius = radius
 
-s = square(4)
-print("Area of the Square : ", s.area())
+    def area(self) :
+        d=  3.14159 * self.radius ** 2   
+        print("Area of the Circle : ", d)
+
+    def data(self) :
+        print("Radius : ",self.radius) 
+
+r = Rectangle(5,10)
+t = Triangle(5,10)
+s = Square(4)
+ci = Circle(7)
+
+for shape in (r,t,s,ci) :
+    shape.data()
+    shape.area()
