2fd1af1d278376def26479c81bbac20b2f60f613
Ruban <trmgaming02@gmail.com>
2025-12-09 19:32:20 +0530

    data structure

---
+++ data structure/algorithms.py
@@ -0,0 +1,24 @@
+#single line algorithm
+def fun1(n):
+    return n*(n+1)/2
+
+print(fun1(4))
+
+#loop algorithm
+
+def fun2(n):
+
+    sum=0
+    for i in range(1,n+1):
+       sum+=i
+       return sum
+print(fun2(4))
+
+#nested loop algorithm
+def fun3(n):
+    sum=0
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            sum+=1
+            return sum
+print(fun3(4))
