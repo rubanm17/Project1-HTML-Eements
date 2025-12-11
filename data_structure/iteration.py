95943a7826a33b91253c3eed7ae2a612d3f4a67b
Ruban <trmgaming02@gmail.com>
2025-12-11 19:27:34 +0530

    data_structure

---
+++ data_structure/iteration.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    literation = 0
+    print("total entered by user is :", n)
+    literation +=1
+    print("total number of literation done by the comp is :", n)
+printnumber(10)
+printnumber(200)
+print("\n with any 'n' the time by the computer will not change")
