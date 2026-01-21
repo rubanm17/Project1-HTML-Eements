e82661fa242d8ca2616762cd5d575d7909601054
Ruban <trmgaming02@gmail.com>
2026-01-21 21:41:06 +0530

    SQL

--- SQL/act1.sql
+++ SQL/act1.sql
@@ -7,7 +7,7 @@ CREATE TABLE supplier (
 
 INSERT INTO supplier (SNO, SNAME, STATUS, CITY) VALUES
 ('S1', 'Smith', 20, 'London'),
-(' S2', 'Jones', 10, 'Paris'),
+('S2', 'Jones', 10, 'Paris'),
 ('S3', 'Blake', 30, 'Paris'),
 ('S4', 'Clark', 20, 'London'),
 ('S5', 'Adams', 30, 'Athens');
