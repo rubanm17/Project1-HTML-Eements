3b5aeb107a68bcbe6b691871f9f502f77ff126f0
Ruban <trmgaming02@gmail.com>
2026-01-27 19:47:34 +0530

    HW_Python

--- HW_Python/SQL_HW/table.sql
+++ HW_Python/SQL_HW/table.sql
@@ -1,3 +1,29 @@
-SELECT *
+CREATE TABLE If NOT EXISTS customers (
+    Name TEXT,
+    COUNTRY TEXT,
+    City TEXT,
+    AGE TEXT,
+    Grade INTEGER
+);
+
+INSERT INTO customers (Name, COUNTRY, City, AGE, Grade) VALUES
+('John ', 'USA', 'New York', '30', 120),
+('Smith', 'Canada', 'Toronto', '25', 95),
+('Sam ', 'USA', 'Los Angeles', '28', 110),
+('Lisa', 'UK', 'London', '32', 85),
+('Tom ', 'USA', 'New York', '40', 130);
+
+SELECT NAME
 FROM customers
-WHERE city = 'New York' OR grade > 100;
+WHERE Grade =
+(SELECT MAX(Grade) FROM customers);
+SELECT AGE
+FROM customers
+WHERE AGE =
+(SELECT MIN(AGE) FROM customers);
+
+SELECT * FROM customers WHERE COUNTRY = 'USA';
+
+SELECT * FROM customers WHERE COUNTRY = 'USA' AND AGE BETWEEN 10 AND 30;
+
+SELECT * FROM customers WHERE NAME LIKE 's%';
