1e957d2a5f23ba978b0442b84385c2f0da3e5eb7
Ruban <trmgaming02@gmail.com>
2026-02-19 19:36:06 +0530

    sqlite3

---
+++ SQL/sqlite3/sqls.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+database = 'database.db'
+
+conn = sqlite3.connect(database)
+print("Opened database successfully")
+import pandas as pd
+tables = pd.read_sql("""SELECT * FROM sqlite_master WHERE type='table';""", conn)
+print(tables)
+matches = pd.read_sql("""SELECT * FROM Match;""", conn)
+print(matches.head())
+
+result1 = pd.read_sql("""SELECT AVG(Win_Margin),Match_Winner FROM Match WHERE Season_ID == 9 GROUP BY Match_Winner ORDER BY AVG(Win_Margin);""", conn)
+print(result1)
+result2 = pd.read_sql("""SELECT COUNT(DISTINCT Venue_ID) FROM Match WHERE Season_ID == 9;""", conn)
+print(result2)
+result3 = pd.read_sql("""SELECT MIN(Win_Margin) ,MAX(Win_Margin),AVG(Win_Margin),COUNT(DISTINCT(Man_of_the_Match)) FROM Match ;""", conn)
+print(result3)
+result4 = pd.read_sql("""SELECT SUM(Win_Margin) FROM Match WHERE Season_ID == 9);""", conn)
+print(result4)
