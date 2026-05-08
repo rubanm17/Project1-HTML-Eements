1c3457158fadab8d4ca05d58595aeea55069ea63
Ruban <trmgaming02@gmail.com>
2026-05-08 12:24:24 +0530

    statistics

--- Python/statistics/mm_data.py
+++ Python/statistics/mm_data.py
@@ -12,15 +12,15 @@ nominal_cat = ['Name','Ticket','Cabin']
 
 original_cat = ['Embarked','Gender']
 
-data['Gender'].value_counts
+data['Gender'].value_counts()
 gender_categories = ['Male','Female']
-data['Gender'] = pd.catagorical(data['Gender'],gender_catagories,ordered = True)
+data['Gender'] = pd.Categorical(data['Gender'],gender_categories,ordered = True)
 median_index = np.median(data['Gender'].cat.codes)
-median_gender = gender_catogories[int(median_index)]
+median_gender = gender_categories[int(median_index)]
 print(median_gender)
-data['Embarked'].value_counts
-embarked_catagories = ['S','C','Q']
-data['Embarked'] = pd.Categorical(data['Embarked'],embarked_catagories,ordered = True)
+data['Embarked'].value_counts()
+embarked_categories = ['S','C','Q']
+data['Embarked'] = pd.Categorical(data['Embarked'],embarked_categories,ordered = True)
 median_index = np.median(data['Embarked'].cat.codes)
-median_embarked = embarked_catagories[int(median_index)]
+median_embarked = embarked_categories[int(median_index)]
 print(median_embarked)
