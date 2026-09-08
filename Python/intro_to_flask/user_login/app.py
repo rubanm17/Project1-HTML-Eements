d209e69d518b1ef5f171f89fc819f761375d6eb5
Ruban <trmgaming02@gmail.com>
2026-09-08 19:26:04 +0530

    Python/intro_to_flask/user_login

--- Python/intro_to_flask/user_login/app.py
+++ Python/intro_to_flask/user_login/app.py
@@ -39,3 +39,41 @@ def login():
 @app.route('/logout')
 def logout():
     return render_template('login.html', msg='Logged out successfully!')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="Flask_login"
+        )
+
+        mycursor = mydb.cursor()
+        mycursor.execute('SELECT * FROM LoginDetails WHERE Name=%s OR Email=%s', (username, email))
+        account = mycursor.fetchone()
+
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            mycursor.execute('INSERT INTO LoginDetails (Name, Password, Email) VALUES (%s, %s, %s)', 
+                            (username, password, email))
+            mydb.commit()
+            msg = 'You have successfully registered!'
+
+    return render_template('register.html', msg=msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
